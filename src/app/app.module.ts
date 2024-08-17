f4073288a2bb8c041eaedcaea28d4c1540218038
Peter Dinis <peterdinis0@pobox.sk>
2024-08-17 17:39:39 +0200

    infra: Updating app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { join } from 'path';
 import { BookModule } from 'src/book/book.module';
 import { CategoryModule } from 'src/category/category.module';
+import { AppGraphqlModule } from 'src/graphql/graphql.module';
 
 @Module({
   imports: [
     PrismaModule,
     BookModule,
     CategoryModule,
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    }),
+    AppGraphqlModule,
   ],
   controllers: [AppController],
   providers: [AppService],
