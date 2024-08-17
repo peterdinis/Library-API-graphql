3b79cefd821836cb65d1cdbc9db234afbeaecdde
Peter Dinis <peterdinis0@pobox.sk>
2024-08-17 19:19:11 +0200

    FOO

--- src/category/dto/pagination-category-types.ts
+++ src/category/dto/pagination-category-types.ts
@@ -1,3 +1,5 @@
+import { ObjectType } from '@nestjs/graphql';
 import { PaginationBookType } from 'src/book/dto/pagination-book-type';
 
+@ObjectType()
 export class PaginationCategoryType extends PaginationBookType {}
