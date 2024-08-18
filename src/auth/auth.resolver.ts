10db9ad8aba888b30e797815d10acdcef0c048b9
Peter Dinis <peterdinis0@pobox.sk>
2024-08-18 19:45:05 +0200

    FOO

---
+++ src/auth/auth.resolver.ts
@@ -0,0 +1,37 @@
+import { UnauthorizedException, UseGuards } from '@nestjs/common';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { GqlAuthGuard } from './guards/gql-auth-guard';
+import { User } from '@prisma/client';
+import { LoginUserType } from './dto/login-user.dto';
+import { RegisterUserType } from './dto/register-user.dto';
+
+@Resolver(() => User)
+export class UserResolver {
+  constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(GqlAuthGuard)
+  @Query(() => User)
+  async me(@Args('email') email: string) {
+    return this.authService.validateUser({ email });
+  }
+
+  @Mutation(() => String)
+  async login(@Args('email') email: string, @Args('password') password: string) {
+    const user = await this.authService.validateUser({ email, password });
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    const { access_token } = await this.authService.login(user);
+    return access_token;
+  }
+
+  @Mutation(() => User)
+  async register(
+    @Args('email') email: string,
+    @Args('password') password: string,
+    @Args('name', { nullable: true }) name?: string,
+  ) {
+    return this.authService.register({ email, password, name });
+  }
+}
