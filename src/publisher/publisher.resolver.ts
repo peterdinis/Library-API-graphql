96ecdeaab793b70bbef23ac7f1ecf2b8b32ad48b
Peter Dinis <peterdinis0@pobox.sk>
2024-08-18 17:06:25 +0200

    FOO

---
+++ src/publisher/publisher.resolver.ts
@@ -0,0 +1,42 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { PublisherService } from './publisher.service';
+import { Publisher } from '@prisma/client';
+import { CreatePublisherInput } from './dto/create-publisher-type';
+import { UpdatePublisherInput } from './dto/update-publisher-type';
+
+@Resolver('Publisher')
+export class PublisherResolver {
+  constructor(private readonly publisherService: PublisherService) {}
+
+  @Query('publishers')
+  async getPublishers(
+    @Args('skip', { type: () => Number, nullable: true }) skip?: number,
+    @Args('take', { type: () => Number, nullable: true }) take?: number,
+    @Args('search', { type: () => String, nullable: true }) search?: string,
+  ): Promise<Publisher[]> {
+    return this.publisherService.getPublishers(skip, take, search);
+  }
+
+  @Query('publisher')
+  async getPublisher(@Args('id', { type: () => Number }) id: number): Promise<Publisher> {
+    return this.publisherService.getPublisher(id);
+  }
+
+  @Mutation('createPublisher')
+  async createPublisher(@Args('data') data: CreatePublisherInput): Promise<Publisher> {
+    return this.publisherService.createPublisher(data);
+  }
+
+  @Mutation('updatePublisher')
+  async updatePublisher(
+    @Args('id', { type: () => Number }) id: number,
+    @Args('data') data: UpdatePublisherInput,
+  ): Promise<Publisher> {
+    return this.publisherService.updatePublisher(id, data);
+  }
+
+  @Mutation('deletePublisher')
+  async deletePublisher(@Args('id', { type: () => Number }) id: number): Promise<Publisher> {
+    return this.publisherService.deletePublisher(id);
+  }
+}
