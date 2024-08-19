daa0c078ddaf503e03cb8e33eec3a1049a91bb7d
Peter Dinis <peterdinis0@pobox.sk>
2024-08-19 17:53:32 +0200

    feat: Finish booking resolver

--- src/booking/booking.resolver.ts
+++ src/booking/booking.resolver.ts
@@ -1,5 +1,36 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+import { BookingService } from './booking.service';
+import { CreateBookingType } from './dto/create-booking-dto';
+import { ReturnBookingType } from './dto/return-booking-dto';
+import { ExtendedBookingType } from './dto/extended-booking-dto';
 import { BookingModel } from './booking.model';
 
 @Resolver(() => BookingModel)
-export class BookingResolver {}
+export class BookingResolver {
+    constructor(private readonly bookingService: BookingService) {}
+
+    @Query(() => [BookingModel])
+    async getAllBookings() {
+        return this.bookingService.getAllBookings();
+    }
+
+    @Query(() => BookingModel)
+    async getOneBooking(@Args('id', { type: () => Int }) id: number) {
+        return this.bookingService.getOneBooking(id);
+    }
+
+    @Mutation(() => BookingModel)
+    async createNewBooking(@Args('bookingDto') bookingDto: CreateBookingType) {
+        return this.bookingService.createNewBooking(bookingDto);
+    }
+
+    @Mutation(() => BookingModel)
+    async returnBooking(@Args('returnDto') returnDto: ReturnBookingType) {
+        return this.bookingService.returnBooking(returnDto);
+    }
+
+    @Mutation(() => BookingModel)
+    async extendedBooking(@Args('extendDto') extendDto: ExtendedBookingType) {
+        return this.bookingService.extendedBooking(extendDto);
+    }
+}
