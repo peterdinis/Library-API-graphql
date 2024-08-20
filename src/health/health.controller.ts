1a0091ba134c2785d42225a6a48ba07f9942b999
Peter Dinis <peterdinis0@pobox.sk>
2024-08-20 11:08:28 +0200

    feat: Update health module

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthCheckService, HttpHealthIndicator, HealthCheck } from '@nestjs/terminus';
+import { PrismaHealthIndicator } from './prisma.health.service';
 
 @Controller('health')
 export class HealthController {
   constructor(
     private health: HealthCheckService,
     private http: HttpHealthIndicator,
+    private prisma: PrismaHealthIndicator,
   ) {}
 
   @Get()
@@ -13,6 +15,7 @@ export class HealthController {
   check() {
     return this.health.check([
       () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+      () => this.prisma.isHealthy('prisma'),
     ]);
   }
 }
--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,11 +1,13 @@
-
+import { PrismaModule } from './../prisma/prisma.module';
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 import { HttpModule } from '@nestjs/axios';
 import { HealthController } from './health.controller';
+import { PrismaHealthIndicator } from './prisma.health.service';
 
 @Module({
-  imports: [TerminusModule, HttpModule],
+  imports: [PrismaModule, TerminusModule, HttpModule],
   controllers: [HealthController],
+  providers: [PrismaHealthIndicator]
 })
 export class HealthModule {}
