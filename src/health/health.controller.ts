434c0de1799794ee792ae6f8d5a7676f22bee9b6
Peter Dinis <peterdinis0@pobox.sk>
2024-08-20 11:17:19 +0200

    FOO

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,21 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheckService, HttpHealthIndicator, HealthCheck } from '@nestjs/terminus';
+import {
+    HealthCheckService,
+    HttpHealthIndicator,
+    HealthCheck,
+    MemoryHealthIndicator,
+} from '@nestjs/terminus';
 import { PrismaHealthIndicator } from './prisma.health.service';
 
 @Controller('health')
 export class HealthController {
-  constructor(
-    private health: HealthCheckService,
-    private http: HttpHealthIndicator,
-    private prisma: PrismaHealthIndicator,
-  ) {}
+    constructor(
+        private health: HealthCheckService,
+        private http: HttpHealthIndicator,
+        private prisma: PrismaHealthIndicator,
+        private memory: MemoryHealthIndicator,
+    ) {}
 
-  @Get()
-  @HealthCheck()
-  check() {
-    return this.health.check([
-      () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
-      () => this.prisma.isHealthy('prisma'),
-    ]);
-  }
+    @Get()
+    @HealthCheck()
+    check() {
+        return this.health.check([
+            () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+            () => this.prisma.isHealthy('prisma'),
+            () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
+        ]);
+    }
 }
--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -6,8 +6,15 @@ import { HealthController } from './health.controller';
 import { PrismaHealthIndicator } from './prisma.health.service';
 
 @Module({
-  imports: [PrismaModule, TerminusModule, HttpModule],
-  controllers: [HealthController],
-  providers: [PrismaHealthIndicator]
+    imports: [
+        PrismaModule,
+        TerminusModule.forRoot({
+            logger: true,
+            errorLogStyle: 'pretty',
+        }),
+        HttpModule,
+    ],
+    controllers: [HealthController],
+    providers: [PrismaHealthIndicator],
 })
 export class HealthModule {}
