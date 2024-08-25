d10abb02b655226d083f9644a6e6043e6ef218b2
Peter Dinis <peterdinis0@pobox.sk>
2024-08-25 21:46:47 +0200

    fix: Fixing bug in booking.module

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -4,10 +4,14 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { BookingService } from './booking.service';
 import { BookingResolver } from './booking.resolver';
 import { BookModule } from 'src/book/book.module';
+import { PubSub } from 'graphql-subscriptions';
 
 @Module({
     imports: [PrismaModule, AuthModule, BookModule],
-    providers: [BookingService, BookingResolver],
-    exports: [BookingService],
+    providers: [BookingService, BookingResolver, {
+        provide: "PUB_SUB",
+        useValue: new PubSub()
+    }],
+    exports: [BookingService, 'PUB_SUB'],
 })
 export class BookingModule {}
