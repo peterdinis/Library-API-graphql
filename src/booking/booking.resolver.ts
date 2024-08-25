61a2de2cdc70f9627b39b76948e0c81c51e71f94
Peter Dinis <peterdinis0@pobox.sk>
2024-08-25 17:44:04 +0200

    FOO

--- src/booking/booking.resolver.ts
+++ src/booking/booking.resolver.ts
@@ -12,7 +12,7 @@ import { PubSub } from 'graphql-subscriptions';
 export class BookingResolver {
     constructor(
         private readonly bookingService: BookingService,
-        @Inject("PUB_SUB") private readonly pubSub: PubSub
+        @Inject('PUB_SUB') private readonly pubSub: PubSub
     ) {}
 
     @Query(() => [BookingModel])
@@ -27,11 +27,19 @@ export class BookingResolver {
 
     @Mutation(() => BookingModel)
     async createNewBooking(@Args('bookingDto') bookingDto: CreateBookingType) {
-        return this.bookingService.createNewBooking(bookingDto);
+        const newBooking = await this.bookingService.createNewBooking(bookingDto);
+        await this.pubSub.publish('bookingCreated', { bookingCreated: newBooking });
+        return newBooking;
     }
 
     @Subscription(() => BookingModel, {
-        resolve: (value) => value.bookingCreated,
+        resolve: (value) => {
+            const booking = value.bookingCreated;
+            return {
+                ...booking,
+                message: `A new booking for the book "${booking.bookName}" was created by user ID ${booking.userId}.`
+            };
+        },
     })
     bookingCreated() {
         return this.pubSub.asyncIterator('bookingCreated');
@@ -39,25 +47,51 @@ export class BookingResolver {
 
     @Mutation(() => BookingModel)
     async returnBooking(@Args('returnDto') returnDto: ReturnBookingType) {
-        return this.bookingService.returnBooking(returnDto);
+        const returnedBooking = await this.bookingService.returnBooking(returnDto);
+        await this.pubSub.publish('bookingReturned', { bookingReturned: returnedBooking });
+        return returnedBooking;
+    }
+
+    @Subscription(() => BookingModel, {
+        resolve: (value) => {
+            const booking = value.bookingReturned;
+            return {
+                ...booking,
+                message: `The booking for the book "${booking.bookName}" by user ID ${booking.userId} has been returned.`
+            };
+        },
+    })
+    bookingReturned() {
+        return this.pubSub.asyncIterator('bookingReturned');
     }
 
     @Mutation(() => BookingModel)
     async extendedBooking(@Args('extendDto') extendDto: ExtendedBookingType) {
-        return this.bookingService.extendedBooking(extendDto);
+        const extendedBooking = await this.bookingService.extendedBooking(extendDto);
+        await this.pubSub.publish('bookingExtended', { bookingExtended: extendedBooking });
+        return extendedBooking;
+    }
+
+    @Subscription(() => BookingModel, {
+        resolve: (value) => {
+            const booking = value.bookingExtended;
+            return {
+                ...booking,
+                message: `The booking for the book "${booking.bookName}" by user ID ${booking.userId} has been extended until ${booking.extendedDate}.`
+            };
+        },
+    })
+    bookingExtended() {
+        return this.pubSub.asyncIterator('bookingExtended');
     }
 
     @Query(() => [BookingModel])
-    async paginationBooking(
-        @Args('paginationDto') paginationDto: PaginationBookingType,
-    ) {
+    async paginationBooking(@Args('paginationDto') paginationDto: PaginationBookingType) {
         return this.bookingService.paginationBooking(paginationDto);
     }
 
     @Query(() => [BookingModel])
-    async searchForBookings(
-        @Args('keyword', { type: () => String }) keyword: string,
-    ) {
+    async searchForBookings(@Args('keyword', { type: () => String }) keyword: string) {
         return this.bookingService.searchForBookings(keyword);
     }
 }
