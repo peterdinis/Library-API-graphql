57bce7fb0c2e80b3bcee90799f20edbc9c958da7
Peter Dinis <peterdinis0@pobox.sk>
2024-08-25 16:42:31 +0200

    feat: Update resolver

--- src/admin/admin.resolver.ts
+++ src/admin/admin.resolver.ts
@@ -1,31 +1,67 @@
-import { Resolver, Mutation } from '@nestjs/graphql';
+import { Resolver, Mutation, Query } from '@nestjs/graphql';
 import { AdminService } from './admin.service';
+import { Response } from 'express';
+import { Res } from '@nestjs/common';
 
 @Resolver()
 export class AdminResolver {
-    constructor(private readonly adminService: AdminService) {}
+  constructor(private readonly adminService: AdminService) {}
 
-    @Mutation(() => String)
-    async downloadBookAsSheets(): Promise<string> {
-        const buffer = await this.adminService.downloadBookAsSheets();
-        return buffer.toString('base64');
-    }
+  @Mutation(() => Boolean)
+  async deleteAllAuthors(): Promise<boolean> {
+    await this.adminService.deleteAllAuthors();
+    return true;
+  }
 
-    @Mutation(() => String)
-    async downloadStudentsAsSheets(): Promise<string> {
-        const buffer = await this.adminService.downloadStudentsAsSheets();
-        return buffer.toString('base64');
-    }
+  @Mutation(() => Boolean)
+  async deleteAllPublishers(): Promise<boolean> {
+    await this.adminService.deleteAllPublishers();
+    return true;
+  }
 
-    @Mutation(() => String)
-    async downloadTeacherAsSheets(): Promise<string> {
-        const buffer = await this.adminService.downloadTeacherAsSheets();
-        return buffer.toString('base64');
-    }
+  @Mutation(() => Boolean)
+  async deleteAllCategories(): Promise<boolean> {
+    await this.adminService.deleteAllCategories();
+    return true;
+  }
 
-    @Mutation(() => String)
-    async downloadBookingAsSheets(): Promise<string> {
-        const buffer = await this.adminService.downloadBookingAsSheets();
-        return buffer.toString('base64');
-    }
+  @Query(() => Boolean)
+  async downloadBooksAsSheets(@Res() res: Response): Promise<void> {
+    const buffer = await this.adminService.downloadBookAsSheets();
+    res.set({
+      'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'Content-Disposition': 'attachment; filename="books.xlsx"',
+    });
+    res.end(buffer);
+  }
+
+  @Query(() => Boolean)
+  async downloadStudentsAsSheets(@Res() res: Response): Promise<void> {
+    const buffer = await this.adminService.downloadStudentsAsSheets();
+    res.set({
+      'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'Content-Disposition': 'attachment; filename="students.xlsx"',
+    });
+    res.end(buffer);
+  }
+
+  @Query(() => Boolean)
+  async downloadTeachersAsSheets(@Res() res: Response): Promise<void> {
+    const buffer = await this.adminService.downloadTeacherAsSheets();
+    res.set({
+      'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'Content-Disposition': 'attachment; filename="teachers.xlsx"',
+    });
+    res.end(buffer);
+  }
+
+  @Query(() => Boolean)
+  async downloadBookingsAsSheets(@Res() res: Response): Promise<void> {
+    const buffer = await this.adminService.downloadBookingAsSheets();
+    res.set({
+      'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      'Content-Disposition': 'attachment; filename="bookings.xlsx"',
+    });
+    res.end(buffer);
+  }
 }
