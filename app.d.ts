7bf75b892ecaf2d77183ef34240b8a49d9130848
Peter Dinis <peterdinis0@pobox.sk>
2024-09-11 13:37:56 +0200

    FOO

--- app.d.ts
+++ app.d.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 declare global {
   namespace Express {
     interface Request {
-      user?: any; // You can replace `any` with a more specific type if you have one
+      user?: any; 
     }
   }
 }
